2e2ef998d1e0616a4843409bf8aafccce3430c81
vinhvinh450 <vinhvinh450@gmail.com>
2025-12-08 12:06:13 +0700

    Shop

---
+++ ClothesShop/Controllers/ShopController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ClothesShop.Data;
+using ClothesShop.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Trang Shop - hiển thị danh sách sản phẩm
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Product
+                .Include(p => p.Category)
+                .Include(p => p.ProductImages)
+                .ToListAsync();
+
+            return View(products);
+        }
+
+    }
+}
